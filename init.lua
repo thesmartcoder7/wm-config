df92cf6ca44650ccf177c01f2a91f2d91061324d
Samuel <samuel.martins4.sm@gmail.com>
2024-10-29 13:41:01 +0300

    chore: include neovim setup

---
+++ init.lua
@@ -0,0 +1,63 @@
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+
+-- add editor transparency
+vim.cmd[[hi Normal guibg=#00000095]]
+vim.cmd[[hi NonText guibg=#00000095]]
+
+
+require('nvim-tree').setup({
+  view = {
+    width = 30,  -- Set the width of the side panel
+    side = "left",  -- Position the panel on the left
+  },
+  renderer = {
+    highlight_opened_files = "all",  -- Highlight opened files in the panel
+  },
+  filters = {
+    dotfiles = false,  -- Show dotfiles by default
+  },
+})
+
+
+-- add nvimtree transparency
+vim.cmd([[
+  highlight NvimTreeNormal guibg=NONE ctermbg=NONE
+  highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
+]])
+
